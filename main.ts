0e17633392955836fb67cebdef0fb952ad2a6f31
phoenixvane <32545460+phoenixvane@users.noreply.github.com>
2020-07-25 15:06:24 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,10 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     うごく = 0
+    pins.servoWritePin(AnalogPin.P0, うごく * 10)
 })
 let うごく = 0
-うごく = 0
+うごく = -1
 basic.forever(function () {
     basic.showString("" + (うごく))
 })
